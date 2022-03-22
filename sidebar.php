bc086111921de033aefebcd2935247e144dccbd1
raddick <raddick@jhu.edu>
2022-03-22 15:12:30 -0400

    Fixed search box styling

--- sidebar.php
+++ sidebar.php
@@ -19,8 +19,17 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 		<aside id="secondary" class="widget-area sidebar-1 h-center-upto-md" aria-label="<?php esc_attr_e( 'Sidebar', 'galaxis' ); ?>">
 			<?php 
 			$this_page_url = home_url($wp -> request);
+			$sidebar_values = array();
+			foreach ( $GLOBALS['wp_registered_sidebars'] as $this_sidebar ) {
+				array_push($sidebar_values, substr(ucwords($this_sidebar['id']), strpos(ucwords($this_sidebar['id']), '-')+1));
+			}
+
 			$which = explode('/',substr($this_page_url,strpos($this_page_url, get_site_url())+strlen(get_site_url())+1))[0];
-			$sidebar_to_load = 'sidebar-'.$which;
+			if (in_array($which, $sidebar_values)) {
+				$sidebar_to_load = 'sidebar-'.$which;
+			} else {
+				$sidebar_to_load = 'sidebar-1';
+			}
 			dynamic_sidebar( $sidebar_to_load ); 
 			if (array_reverse(explode('/',$this_page_url))[0] == 'science') { ?>
 				<div class='sidebar-toc'>
