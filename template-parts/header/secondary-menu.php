6ac0ec79f381809c2a80bfbe4958e8c46a69abbb
raddick <raddick@jhu.edu>
2022-12-30 07:29:06 -0500

    v2.2, non-optimal solution to display secondary menu on small screens

--- template-parts/header/secondary-menu.php
+++ template-parts/header/secondary-menu.php
@@ -1,22 +1,17 @@
 
 <?php if ( has_nav_menu( 'menu-2' ) & preg_match('/(dr\d|data)/i',get_permalink())  ) {  ?>
 
-		<nav id="secondary-navigation" class="main-navigation wrapper">
+		<nav id="secondary-navigation" class="wrapper">
 
 			<?php if ( has_nav_menu( 'menu-2' ) ) { ?>
-<!--				<button type="button" class="menu-button menu-toggle" aria-controls="secondary-menu" aria-expanded="false">
-					<span class="screen-reader-text"><?php esc_html_e( 'Secondary Menu', 'galaxis' ); ?></span>
-					<span class="main-navigation__icon">
-						<span class="main-navigation__icon__middle"></span>
-					</span>
-				</button>-->
 				<?php
 				wp_nav_menu(
 					array(
 						'theme_location'  => 'menu-2',
 						'depth'           => 3,
-						'menu_id'         => 'secondary-menu',
-						'container_class' => 'secondary-menu-container',
+						//'menu_id'         => 'secondary-menu',
+						'menu_class'	  => 'secondary-menu',
+						'container_class' => 'secondary-menu-container'
 						//'walker'          => new Galaxis_Primary_Walker_Nav_Menu(),
 					)
 				);
