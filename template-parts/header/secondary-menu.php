4cbc66c52a290bbbdce56e9b059957897111f95d
Jordan Raddick <raddick@jhu.edu>
2024-06-10 14:29:03 -0600

    menu items point within previous data release pages

--- template-parts/header/secondary-menu.php
+++ template-parts/header/secondary-menu.php
@@ -20,37 +20,37 @@
 		<div class="secondary-menu-container">
 			<ul id="menu-data-release-19" class="secondary-menu">
 				<li id="menu-item-4010" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item menu-item-4010">
-					<a href="/dr19/" aria-current="page"><strong>DR<?php echo $this_dr; ?></strong></a>
+					<a href="/dr<?php echo $this_dr; ?>/" aria-current="page"><strong>DR<?php echo $this_dr; ?></strong></a>
 				</li>
 				<li id="menu-item-550" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-550">
-					<a href="https://dev.sdss.org/dr18/data_access/">Data Access</a>
+					<a href="/dr<?php echo $this_dr; ?>/data_access/">Data Access</a>
 				</li>
 				<li id="menu-item-543" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-543">
-					<a href="https://dev.sdss.org/dr18/bhm/">BHM</a>
+					<a href="/dr<?php echo $this_dr; ?>/bhm/">BHM</a>
 				</li>
 				<li id="menu-item-541" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-541">
-					<a href="https://dev.sdss.org/dr18/mwm/">MWM</a>
+					<a href="/dr<?php echo $this_dr; ?>/mwm/">MWM</a>
 				</li>
 				<li id="menu-item-2403" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2403">
-					<a href="https://dev.sdss.org/dr18/lvm/about/">LVM</a>
+					<a href="/dr<?php echo $this_dr; ?>/lvm/about/">LVM</a>
 				</li>
 				<li id="menu-item-548" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-548">
-					<a href="https://dev.sdss.org/dr18/imaging/">Imaging</a>
+					<a href="/dr<?php echo $this_dr; ?>/imaging/">Imaging</a>
 				</li>
 				<li id="menu-item-549" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-549">
-					<a href="https://dev.sdss.org/dr18/completed_surveys/"><span class="tooltip"><span class="anchortext">Completed Surveys</span><div class="tooltiptext" id="completed">APOGEE, (e)BOSS,<br>MaNGA, SDSS-I/-II</div></span></a>
+					<a href="/dr<?php echo $this_dr; ?>/completed_surveys/"><span class="tooltip"><span class="anchortext">Completed Surveys</span><div class="tooltiptext" id="completed">APOGEE, (e)BOSS,<br>MaNGA, SDSS-I/-II</div></span></a>
 				</li>
 				<li id="menu-item-544" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-544">
-					<a href="https://dev.sdss.org/dr18/targeting/">Targeting</a>
+					<a href="/dr<?php echo $this_dr; ?>/targeting/">Targeting</a>
 				</li>
 				<li id="menu-item-545" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-545">
-					<a href="https://dev.sdss.org/dr18/software/">Software</a>
+					<a href="/dr<?php echo $this_dr; ?>/software/">Software</a>
 				</li>
 				<li id="menu-item-547" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-547">
-					<a href="https://dev.sdss.org/dr18/tutorials/">Tutorials</a>
+					<a href="/dr<?php echo $this_dr; ?>/tutorials/">Tutorials</a>
 				</li>
 				<li id="menu-item-546" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-546">
-					<a href="https://dev.sdss.org/dr18/help/">Help</a>
+					<a href="/dr<?php echo $this_dr; ?>/help/">Help</a>
 				</li>
 			</ul>
 		</div>	
